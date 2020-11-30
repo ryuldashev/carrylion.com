2b2f137fa178fcfd4e4866b6d826c2b3251fbb0d
Ruslan Yuldashev <ruslan@dcode.team>
2020-11-30 23:43:14 +0500

    Upd design & demoreel

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@
   'use strict';
 
   new WOW().init();
+
+  var nav = $('.navbar');
+  function scrollClass() {
+      nav.toggleClass('scrolled', $(window).scrollTop() >= 150);
+  }
+  $(window).scroll(scrollClass);
+  scrollClass();
   
 })();
